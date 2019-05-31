0d07316fc7327051e8b22422ef6648d9af56244f
fulvi <fulvi@DESKTOP-9ELD0TJ>
2019-05-31 00:02:57 -0300

    form builder beta

---
+++ application/config/homolog/pagination.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//The following is a list of all the preferences you can pass to the initialization function to tailor the display.
