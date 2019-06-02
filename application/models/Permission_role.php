2ac403dfa14131f33c17af4ca42216f2da9c180a
fulvi <fulvi@DESKTOP-9ELD0TJ>
2019-06-02 14:13:19 -0300

    migrate

---
+++ application/models/Permission_role.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of role
+ *
+ * @author fulvi
+ */
+class Permission_role extends BM_Model{
+    public const TABLE_NAME = 'permission_role';
+    public const JOIN_TABLES = [
+        ['table' => permission::TABLE_NAME, 'on' => 'id'],
+        ['table' => role::TABLE_NAME, 'on' => 'id'],
+    ];
+}
