e0831af62f7e6aa218fa68fe6a9b957b1480177d
fulvi <fulvi@DESKTOP-9ELD0TJ>
2019-06-02 13:36:28 -0300

    migrate

---
+++ application/language/english/controller/User_maintence_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$lang['System Users']='System Users';
+$lang['Edit User']='Edit User';
+$lang['New User']='New User';
+$lang['New']='New';
